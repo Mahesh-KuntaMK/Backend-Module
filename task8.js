9cd7320545f37e791043dc545540234845643119
Mahesh-KuntaMK <k.maheshkunta@gmail.com>
2023-09-06 23:00:45 +0530

    cleaning upthe code task9

--- task8.js
+++ task8.js
@@ -1,56 +1,7 @@
 const http=require('http');
-const fs=require('fs');
 
-const server=http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers) 
-     
-    const url=req.url;
-    const method=req.method;
-    //let message;
-    if(url==='/'){
-       // res.setHeader('Content-Type','text/html');
-        fs.readFile('message.text',{encoding:"utf-8"},(err,data)=>{
-            if(err){
-                console.log(err);
-            }
-            res.write('<html>');
-            res.write('<head><title>My first page</title></head>');
-            res.write(`<body>${data}</body>`)
-            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-            res.write('</html>');
-            return  res.end();
-        })
-       
-    }
-  else if(url==="/message" && method==="POST"){
-        const body=[];
-        req.on('data',(chuck)=>{
-            body.push(chuck)
-            console.log(body)
-        })
-        return   req.on('end',(chuck)=>{
-            const parseBody=Buffer.concat(body).toString();
-            const message=parseBody.split('=')[1]
-            fs.writeFile('message.text',message,(err)=>{
-                res.statusCode=302;
-                res.setHeader('location',"/")
-                
-              
-                return res.end();
-            })
-              
-      
-        })
-      
-    }
-else{
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body><h1>Hello from my node.js</h1></body>');
-    res.write('</html>');
-    res.end();
-}  
+const routes=require('./routes');
 
-})
-server.listen(4000)     
+const server=http.createServer(routes)
+
+server.listen(3000)     
