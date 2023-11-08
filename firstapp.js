0b1ef3084cbfad8fb9511c867bfedfd5c422ef70
Mahesh-KuntaMK <k.maheshkunta@gmail.com>
2023-11-08 15:43:10 +0530

    intro to middlewares and route paths

--- firstapp.js
+++ firstapp.js
@@ -2,14 +2,25 @@
 const express=require('express');
 
 let app=express();
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req,res,next)=>{
-        console.log("this is first middle ware")
-        next();
+
+// app.use('/',(req,res,next)=>{
+//         console.log("this is first middle ware")
+//         next();
+// })
+app.use('/app-product',(req,res,next)=>{
+   // console.log("this is another middleware")
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add product</form>');
 })
-app.use((req,res,next)=>{
-    console.log("this is another middleware")
-    res.send('<h1>returns the header as html or text as default in express</h1>')
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+    //console.log("this is first middle ware")
+    res.send('<h1>"hello from express js"</h1>')
 })
  
 app.listen(3000);
