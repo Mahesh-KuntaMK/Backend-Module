9210cc5fbbe6e0731da5ce1035955c1296a944bd
Mahesh-KuntaMK <k.maheshkunta@gmail.com>
2023-11-11 11:52:34 +0530

    done chnages wtih  paths using filters and routers

--- firstapp.js
+++ firstapp.js
@@ -1,26 +1,29 @@
 
-const express=require('express');
 
-let app=express();
+
+
+const express = require('express');
+
+
 const bodyParser=require('body-parser');
+
+const  app=express();
+
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended:false}));
 
 
-// app.use('/',(req,res,next)=>{
-//         console.log("this is first middle ware")
-//         next();
-// })
-app.use('/app-product',(req,res,next)=>{
-   // console.log("this is another middleware")
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add product</form>');
+
+
+  
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
 })
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
-app.use('/',(req,res,next)=>{
-    //console.log("this is first middle ware")
-    res.send('<h1>"hello from express js"</h1>')
-})
- 
+
+
 app.listen(3000);
