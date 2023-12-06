33ed8317b51d2dbc61c65256e4b1b6b19c5e815d
Mahesh-KuntaMK <k.maheshkunta@gmail.com>
2023-12-06 12:44:41 +0530

    serving html,css,js express js 5th task

--- firstapp.js
+++ firstapp.js
@@ -1,6 +1,6 @@
 
 
-
+const path=require('path');
 
 const express = require('express');
 
@@ -11,6 +11,7 @@ const  app=express();
 
 const adminRoutes=require('./routes/admin');
 const shopRoutes=require('./routes/shop');
+const contactusroutes=require('./routes/contact.js')
 
 app.use(bodyParser.urlencoded({extended:false}));
 
@@ -19,10 +20,12 @@ app.use(bodyParser.urlencoded({extended:false}));
 
   
 app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
+app.use(shopRoutes);
+app.use(contactusroutes);
 
+app.use(express.static(path.join(__dirname,'public')))
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 
 
